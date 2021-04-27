57f5556bbfc92179c4a589bbc0601d058ad4cc56
KyuKyu <gtnine999@gmail.com>
2021-04-28 00:04:02 +0900

    ログイン時にログイン情報をジョブで非同期にDBに保存

---
+++ app/Jobs/UserJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\LoginLog;
+
+class UserJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle() :void
+    {
+        $login_log = new LoginLog();
+        $login_log->user_id = $this->user->id;
+        $login_log->name = $this->user->name;
+        $login_log->save();
+    }
+}
